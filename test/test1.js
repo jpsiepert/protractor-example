29847738c9021bbd56bca8969467681138a22f8e
Jennifer <jpsiepert@gmail.com>
2015-07-08 14:21:30 -0600

    updates config tests

--- test/test1.js
+++ test/test1.js
@@ -3,14 +3,14 @@
 describe('clicking login', function() {
 
   it('should log user in', function() {
-    browser.get('http://localhost:8080/#/login');
+    browser.get('/#/login');
 
     element(by.model('user.email')).sendKeys('test5@test5.com');
     element(by.model('user.password')).sendKeys('test');
     element(by.css('[ng-click="login()"]')).click();
     browser.sleep(2000);
 
-    expect(browser.getCurrentUrl()).toBe('http://localhost:8080/#/awesomeness');
+    expect(browser.getCurrentUrl()).toContain('/#/awesomeness');
   });
 
 });
